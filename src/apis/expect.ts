1d6a79e64d1482a0ae8635a5347f26930fe1df67
gaojuqian <jqiown@163.com>
2022-07-25 22:33:21 +0800

    add not

--- src/apis/expect.ts
+++ src/apis/expect.ts
@@ -1,19 +1,24 @@
 import * as jestUtils from "@jest/expect-utils";
+const eq = jestUtils.equals;
 
 /**
  * expect({ a: 123 }).toEqual({ a: 123 }) -> true
- * @param a 
+ * @param a
  * @returns toEqual(b) -> boolean
  */
 export default function (a: any) {
-  try{
-  const eq = jestUtils.equals;
+  try {
     return {
       toEqual(b: any) {
         return eq(a, b);
       },
+      not: {
+        toEqual(b: any) {
+          return !eq(a, b);
+        },
+      },
     };
-  }catch(error){
-    throw(error);
+  } catch (error) {
+    throw error;
   }
 }
